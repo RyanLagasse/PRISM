5c0dc3af63f78c9062b75c4b33f3d4da45ce4b1c
RyanLagasse <ryanplagasse@gmail.com>
2024-11-19 20:06:18 -0500

    PRISM2 initial commit

---
+++ VAE.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from tqdm import tqdm
